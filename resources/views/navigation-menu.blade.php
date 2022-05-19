880af3b57a39d13b578172072b88b0028a24c1b8
Darshan V <dvanakudre@gmail.com>
2022-05-19 19:41:43 +0530

    dropdown

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -13,35 +13,50 @@
                 </div>
                 
                 <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                <div class="hidden space-x-4 sm:-my-px sm:ml-6 sm:flex">
 
                     <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-jet-nav-link>
 
-                    <x-jet-nav-link>
-                       Masters
-                    </x-jet-nav-link>
-
-                    <x-jet-nav-link>
-                        Transactions
-                    </x-jet-nav-link>
-
-                    <x-jet-nav-link>
-                        Fees Details
-                    </x-jet-nav-link>
-                    
-                    <x-jet-nav-link>
-                        Fees Details
-                    </x-jet-nav-link>
-
-                    <x-jet-nav-link>
-                        Report
-                    </x-jet-nav-link>
-
-                    <x-jet-nav-link>
-                       Certificates
-                    </x-jet-nav-link>
+                    <div class="hidden sm:flex sm:items-center sm:ml-0 border-b border-b-blue-700">
+                        <div class="ml-3 relative">
+                            <x-jet-dropdown align="right" width="48">
+                                <x-slot name="trigger">
+                                    <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition">
+                                        Masters
+                                        <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                                            <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                        </svg>
+                                    </button>
+                            </x-slot> 
+        
+                                <x-slot name="content">
+                                    <x-jet-dropdown-link href="{{ route('profile.show') }}">
+                                        {{ __('Profile') }}
+                                    </x-jet-dropdown-link>
+        
+                                    @if (Laravel\Jetstream\Jetstream::hasApiFeatures())
+                                        <x-jet-dropdown-link href="{{ route('api-tokens.index') }}">
+                                            {{ __('API Tokens') }}
+                                        </x-jet-dropdown-link>
+                                    @endif
+        
+                                    <div class="border-t border-gray-100"></div>
+        
+                                    <!-- Authentication -->
+                                    <form method="POST" action="{{ route('logout') }}" x-data>
+                                        @csrf
+        
+                                        <x-jet-dropdown-link href="{{ route('logout') }}"
+                                                 @click.prevent="$root.submit();">
+                                            {{ __('Log Out') }}
+                                        </x-jet-dropdown-link>
+                                    </form>
+                                </x-slot>
+                            </x-jet-dropdown>
+                        </div>
+                    </div>
                 </div>
             </div>
 
